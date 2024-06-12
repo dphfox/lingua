ec3897c1bd4e20a2f5d8aa7f86bb8f9be20e4ea2
Daniel P H Fox <danielphfox@gmail.com>
2024-06-12 18:08:13 +0100

    conditionally compile for wasm32

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+#![cfg(target_arch = "wasm32")]
+
 use std::{cell::RefCell, collections::HashMap, num::Wrapping, panic::{catch_unwind, AssertUnwindSafe, UnwindSafe}};
 
 use serde::{de::DeserializeOwned, Serialize};
