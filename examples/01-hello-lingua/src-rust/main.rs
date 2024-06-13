fe2fb0c4f21629709fa5a4bd36667cab439fc101
Daniel P H Fox <danielphfox@gmail.com>
2024-06-13 01:12:02 +0100

    Tidy up Hello Lingua example on Rust side

--- examples/01-hello-lingua/src-rust/main.rs
+++ examples/01-hello-lingua/src-rust/main.rs
@@ -1,9 +1,23 @@
+// When you include a Rust module in a Luau project with Wasynth, you can't
+// easily send complex data between the two. Extern functions can only send
+// simple numbers.
+
+// Lingua gets around this by letting you turn a complex piece of data into
+// a simple number. You can pass this simple number between Rust and Luau 
+// however you want to. At the final destination, you can turn the simple
+// number back into the original piece of data.
+
+// You don't need to worry about how this works for the most part. As long
+// as your data can be represented neatly as JSON, it'll transfer to the
+// other side just fine.
+
+// -----------------------------------------------------------------------------
+
 #[cfg(not(target_arch = "wasm32"))]
 compile_error!("This project must target WebAssembly to compile correctly.");
 
+use lingua::{receive_from_luau, send_to_luau};
 use serde::{Serialize, Deserialize};
-
-use lingua;
 
 #[derive(Serialize, Deserialize)]
 struct LuauGreeting {
@@ -22,33 +36,18 @@ extern "C" {
 }
 
 fn main() {
+	unsafe {
+		let luau_greeting: LuauGreeting = receive_from_luau(
+			ask_luau_to_say_hello().into()
+		).unwrap();
 
-	// When you include a Rust module in a Luau project with Wasynth, you can't
-	// easily send complex data between the two. Extern functions can only send
-	// simple numbers.
+		let rust_greeting = RustGreeting {
+			in_response_to: luau_greeting,
+			greeting_from_rust: String::from("Hello, Luau!")
+		};
 
-	// Lingua gets around this by letting you turn a complex piece of data into
-	// a simple number. You can pass this simple number between Rust and Luau 
-	// however you want to. At the final destination, you can turn the simple
-	// number back into the original piece of data.
-
-	// You don't need to worry about how this works for the most part. As long
-	// as your data can be represented neatly as JSON, it'll transfer to the
-	// other side just fine.
-
-	
-	let luau_greeting: LuauGreeting = {
-		let handle = unsafe { ask_luau_to_say_hello() }.into();
-		lingua::receive_from_luau(handle).unwrap()
-	};
-
-	let rust_greeting = RustGreeting {
-		in_response_to: luau_greeting,
-		greeting_from_rust: String::from("Hello, Luau!")
-	};
-
-	{
-		let handle = lingua::send_to_luau(&rust_greeting).unwrap();
-		unsafe { respond_to_luau_greeting(handle.into()) };
+		respond_to_luau_greeting(
+			send_to_luau(&rust_greeting).unwrap().into()
+		)
 	}
 }
